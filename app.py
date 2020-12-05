a908282ee51539016aca5c0514e8da70fea2db7f
rhythm244 <63957603+rhythm244@users.noreply.github.com>
2020-12-05 16:17:53 +0700

    before add booststrap

--- app.py
+++ app.py
@@ -7,15 +7,19 @@ db = SQL("sqlite:///money.db")
 
 @app.route("/")
 def index():
-    rows = db.execute("SELECT * FROM money")
+    rows = db.execute("SELECT * FROM finance")
     return render_template("index.html",rows=rows)
+
+""" INSERT INTO table_name (column1, column2, column3, ...)
+VALUES (value1, value2, value3, ...); 
+"""
 
 @app.route("/register", methods=["GET","POST"])
 def register():
     if request.method == "GET":
         return render_template("register.html")
     else:
-        name = request.form.get("name")
+        username = request.form.get("username")
         password = request.form.get("password")
-        db.execute("INSERT ")
+        db.execute("INSERT INTO finance (username, password) VALUES (:username,:password)",username=username,password=password)
     return redirect("/")    
